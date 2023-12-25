a26e162681c0c305d722b672f93b861c0b1cef44
jnshort <justinnshort98@gmail.com>
2023-12-24 22:55:41 -0800

    some stuff still broken

--- day3/versions/main.go
+++ day3/versions/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -31,10 +32,37 @@ func main() {
 		runes[i] = []rune(v)
 	}
 
+	valid_numbers := []rune{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+
 	for i := range runes {
 		for j := range runes[i] {
-			fmt.Print(string(runes[i][j]))
+			for _, v := range valid_numbers {
+				if runes[i][j] == v {
+					var number_string = string(runes[i][j])
+					var k = j + 1
+					for k < len(runes[i]) && contains(valid_numbers, runes[i][k]) {
+						number_string += string(runes[i][k])
+						k++
+					}
+					var number, err = strconv.Atoi(number_string)
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					var length = len(number_string)
+					fmt.Println(number, length)
+
+				}
+			}
 		}
-		fmt.Println()
 	}
+}
+
+func contains(s []rune, e rune) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
